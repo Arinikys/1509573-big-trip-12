96e8064fbe9da14ec8290829cacc8344411ec7c6
Arina <avernidub@ourgapps.com>
2020-09-12 15:29:16 +0300

    add disabled

--- src/view/edit-event-form.js
+++ src/view/edit-event-form.js
@@ -71,6 +71,7 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
           type="checkbox"
           name="event-offer-${optionAttr}"
           ${availableOptionList.has(option.title) ? `checked` : ``}
+          ${isDisabled ? `disabled` : ``}
         >
         <label class="event__offer-label" for="event-offer-${optionAttr}-1">
           <span class="event__offer-title">${option.title}</span>
@@ -89,7 +90,7 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
       if (tripEvent.type === type) {
         let tripEventNameLC = tripEvent.name.toLowerCase();
         tripEventList += `<div class="event__type-item">
-                <input id="event-type-${tripEventNameLC}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${tripEventNameLC}">
+                <input id="event-type-${tripEventNameLC}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${tripEventNameLC}" ${isDisabled ? `disabled` : ``}>
                 <label class="event__type-label  event__type-label--${tripEventNameLC}" for="event-type-${tripEventNameLC}-1">${tripEvent.name}</label>
               </div>`;
       }
@@ -123,7 +124,7 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
             <span class="visually-hidden">Choose event type</span>
             <img class="event__type-icon" width="17" height="17" src="img/icons/${dataEventName}.png" alt="Event type icon">
           </label>
-          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox" ${isDisabled ? `disabled` : ``}>
 
           <div class="event__type-list">
             <fieldset class="event__type-group">
@@ -141,7 +142,7 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
           <label class="event__label  event__type-output" for="event-destination-1">
             ${decoratedDataEventName} ${prep}
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${dataDestinationCity}" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${dataDestinationCity}" list="destination-list-1" ${isDisabled ? `disabled` : ``}>
           <datalist id="destination-list-1">
             ${destinationCityTemplate}
           </datalist>
@@ -151,12 +152,12 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
           <label class="visually-hidden" for="event-start-time-1">
             From
           </label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${prettifyDate(startDate)}">
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${prettifyDate(startDate)}" ${isDisabled ? `disabled` : ``}>
           —
           <label class="visually-hidden" for="event-end-time-1">
             To
           </label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${prettifyDate(endDate)}">
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${prettifyDate(endDate)}" ${isDisabled ? `disabled` : ``}>
         </div>
 
         <div class="event__field-group  event__field-group--price">
@@ -164,13 +165,13 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
             <span class="visually-hidden">Price</span>
             €
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${price}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${price}" ${isDisabled ? `disabled` : ``}>
         </div>
 
         <button class="event__save-btn  btn  btn--blue" ${isDisabled ? `disabled` : ``}> ${isSaving ? `saving...` : `save`}</button>
         <button class="event__reset-btn" type="reset" ${isDisabled ? `disabled` : ``}>${isDeleting ? `deleting...` : `delete`}</button>
 
-        <input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${isFavorite ? `checked` : ``}>
+        <input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${isFavorite ? `checked` : ``} ${isDisabled ? `disabled` : ``}>
         <label class="event__favorite-btn" for="event-favorite-1">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
@@ -178,7 +179,7 @@ const createEditEventTemplate = (curEvent = {}, destinationPoints, offers) => {
           </svg>
         </label>
 
-        <button class="event__rollup-btn" type="button">
+        <button class="event__rollup-btn" type="button" ${isDisabled ? `disabled` : ``}>
           <span class="visually-hidden">Open event</span>
         </button>
       </header>
